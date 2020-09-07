1c16255d2eb013b32fa44a578d21c3bcd5013ae5
Cristian Flores <cristianflores.ee@gmail.com>
2020-09-07 13:24:22 -0300

    add reference repos

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const routes = require('./app/routes');
+const chalk = require('chalk');
+// const routes = require('./app/routes');
 const { PORT, CORS } = require('./app/config');
 
 const app = express();
@@ -9,10 +10,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({ origin: CORS }));
 
-app.use(routes);
+// app.use(routes);
 
 // const server = app.listen(3000);
 
-app.listen(PORT, () => {
-  console.log(`Listening on PORT: ${PORT}`);
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  // console.log(`Listening on PORT: ${PORT}`);
+  console.log(
+    `%s App is running at http://localhost:%d in %s mode`,
+    chalk.green('g'),
+    app.get('port'),
+    app.get('env')
+  );
 });
+
+(async () => {
+  try {
+    //await server.start()
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+})();
